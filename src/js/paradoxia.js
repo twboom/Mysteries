8e4ad7ae8c207e245643be3a09d3c3a9f93c4b2a
Thijs Boom <twboom@hotmail.nl>
2021-03-07 16:25:20 +0100

    Made some progress

--- src/js/paradoxia.js
+++ src/js/paradoxia.js
@@ -5,22 +5,33 @@ paradoxia.proto = [
     'selectorOption.html'
 ]
 
-paradoxia.files = {}
-
 paradoxia.init = function() {
     paradoxia.screen = document.querySelector('main')
     paradoxia.chapter = parseInt(mainframe.parameters('chapter'));
     if (paradoxia.chapter === null) { paradoxia.screen = 'selector' };
+}
 
+paradoxia.fetch = function() {
+    paradoxia.files = {}
     for (let i = 0; i < paradoxia.proto.length; i++) {
         const folder = 'src/assets/protofiles/'
         const file = paradoxia.proto[i];
+        const fileName = file.split('.')[0]
         fetch(folder + file)
             .then(response => response.text())
-            .then(data => paradoxia.files[file] = data)
+            .then(data => paradoxia.files[fileName] = data)
     }
+    fetch('chapters/chapters.json')
+        .then(response => response.json())
+        .then(data => paradoxia.chapters = data);
+
+    fetch('src/assets/consoleArt.txt')
+        .then(response => response.text())
+        .then(data => console.log(data))
 }
 
-fetch('chapters/chapters.json')
-    .then(response => response.json())
-    .then(data => paradoxia.chapters = data)
+paradoxia.fetch()
+
+paradoxia.test = function() {
+    paradoxia.selector()
+}
--- src/js/selector.js
+++ src/js/selector.js
@@ -1,3 +1,17 @@
 paradoxia.selector = function() {
-    
+    document.querySelector('h2.header').innerHTML = 'Chapter Selector'
+    let options = []
+    for (let i = 0; i < paradoxia.chapters.length; i++) {
+        const thisOption = paradoxia.chapters[i]
+        let option = paradoxia.files.selectorOption;
+        console.log(option)
+        option.replace('<!--LevelNumber-->', thisOption.number)
+        option.replace('<!--LevelTitle-->', thisOption.title)
+        options.push(option)
+        console.log(option)
+    }
+    optionString = ''
+    for (let i = 0; i < options.length; i++) {
+        optionString += options[i]
+    }
 }
